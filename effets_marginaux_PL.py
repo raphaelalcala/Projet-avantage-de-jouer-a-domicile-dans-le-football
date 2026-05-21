b9cf84132fd89f084b81d064e28f80a2059631ce
raph <raphaelalcala0@gmail.com>
2026-05-21 17:09:05 -0600

    Update effets_marginaux_PL.py

--- effets_marginaux_PL.py
+++ effets_marginaux_PL.py
@@ -13,3 +13,9 @@ modele = sm.Logit(y, X).fit(disp=0)
 print(modele.summary())
 print("\n--- EFFETS MARGINAUX ---")
 print(modele.get_margeff().summary())
+
+--- EFFETS MARGINAUX ---
+covid           -0.0503      0.025     -2.000      0.046
+home_form        0.0465      0.017      2.765      0.006
+away_form       -0.0538      0.017     -3.169      0.002
+ranking_diff     0.0076      0.001      8.261      0.000
